6a94b56d464c5a39a183f7819f3c854c151f658d
Nguyễn Lê Hà <65054260+nguyenleha@users.noreply.github.com>
2024-09-25 09:01:53 +0700

    [DOCKER] add be and nginx

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
-
   // Versioning
   app.setGlobalPrefix('api');
   app.enableVersioning({
